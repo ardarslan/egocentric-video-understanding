839a649e028dc9deb9a10aacf192d5f71ef0dfb5
Arda Arslan <aarslan@bmicgpu04.ee.ethz.ch>
2023-08-05 15:36:05 +0200

    Added extra clip remover

--- scripts/02_download_data/extra_clip_remover.py
+++ scripts/02_download_data/extra_clip_remover.py
@@ -3,27 +3,23 @@ import json
 import argparse
 
 
-def get_clip_uids(json_file_path):
-    with open(json_file_path, "r") as json_file:
-        json_dict = json.load(json_file)
-    clip_uids = []
-    for video in json_dict["videos"]:
-        for clip in video["clips"]:
-            clip_uids.append(clip["clip_uid"])
-    return clip_uids
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Argument parser")
-    parser.add_argument("--input_json_folder_path", type=str, required=True)
+    parser.add_argument("--annotations_json_file_path", type=str, required=True)
     parser.add_argument("--clips_folder_path", type=str, required=True)
+    parser.add_argument("--egovlp_egonce_features_folder_path", type=str, required=True)
+    parser.add_argument("--internvideo_features_folder_path", type=str, required=True)
+    parser.add_argument("--omnivore_features_folder_path", type=str, required=True)
+    parser.add_argument("--slowfast_features_folder_path", type=str, required=True)
     args = parser.parse_args()
 
-    train_clip_uids = get_clip_uids(os.path.join(args.input_json_folder_path, "moments_train.json"))
-    val_clip_uids = get_clip_uids(os.path.join(args.input_json_folder_path, "moments_val.json"))
-    test_clip_uids = get_clip_uids(os.path.join(args.input_json_folder_path, "moments_test_unannotated.json"))
-    train_val_test_clip_uids = set(train_clip_uids).union(val_clip_uids).union(test_clip_uids)
-
-    for clip_file_name in os.listdir(args.clips_folder_path):
-        if clip_file_name[-4:] == ".mp4" and clip_file_name[:-4] not in train_val_test_clip_uids:
-            os.remove(os.path.join(args.clips_folder_path, clip_file_name))
+    with open(args.annotations_json_file_path, "r") as annotations_json_file:
+        annotations_json_dict = json.load(annotations_json_file)
+        clip_uids = frozenset(annotations_json_dict.keys())
+    
+    for folder_path in [args.clips_folder_path, args.egovlp_egonce_features_folder_path, args.internvideo_features_folder_path, args.omnivore_features_folder_path, args.slowfast_features_folder_path]:
+        for file_name in os.listdir(folder_path):
+            if file_name.split(".")[0] not in clip_uids:
+                os.remove(os.path.join(folder_path, file_name))
+            else:
+                continue
