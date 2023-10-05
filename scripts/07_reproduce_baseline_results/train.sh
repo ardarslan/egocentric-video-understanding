235f95b381bf091f8c0c114a941ff602668e536e
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2023-10-05 19:58:37 +0200

    Updates

--- scripts/07_reproduce_baseline_results/train.sh
+++ scripts/07_reproduce_baseline_results/train.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python train.py --combine_train
+python train.py --combine_train --config ./configs/baseline_1.yaml --output
+python train.py --combine_train --config ./configs/baseline_2.yaml --output
+python train.py --combine_train --config ./configs/baseline_3.yaml --output
+python train.py --combine_train --config ./configs/baseline_4.yaml --output
+python train.py --combine_train --config ./configs/baseline_5.yaml --output
