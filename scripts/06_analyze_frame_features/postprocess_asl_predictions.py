d2b9a191c47eb3f72425acf64578ac3950801c6b
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2023-10-06 07:30:23 +0200

    Updates

--- scripts/06_analyze_frame_features/postprocess_asl_predictions.py
+++ scripts/06_analyze_frame_features/postprocess_asl_predictions.py
@@ -1,14 +1,28 @@
 import os
 import cv2
 import json
-from tqdm import tqdm
+import pickle
+from pqdm import pqdm
 
+from typing import Any, Dict, List
+
+
+asl_predictions_path = os.path.join(
+    os.environ["CODE"],
+    "scripts/07_reproduce_baseline_results/submission_final.json",
+)
+
+analysis_data_path = os.path.join(
+    os.environ["SCRATCH"], "ego4d_data/v2/analysis_data", "analysis_data.pickle"
+)
+
+label_verb_noun_tools_mapping_path = os.path.join(
+    os.environ["CODE"],
+    "scripts/06_analyze_frame_features/label_verb_noun_tool_mapping.json",
+)
 
 with open(
-    os.path.join(
-        os.environ["CODE"],
-        "scripts/06_analyze_frame_features/label_verb_noun_tool_mapping.json",
-    ),
+    label_verb_noun_tools_mapping_path,
     "r",
 ) as reader:
     label_verb_noun_tools_mapping = json.load(reader)
@@ -17,21 +31,23 @@ distinct_ground_truth_labels = ["background"] + sorted(
     list(label_verb_noun_tools_mapping.keys())
 )
 
-clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping = dict()
-
-asl_predictions_path = os.path.join(
-    os.environ["CODE"],
-    "scripts/07_reproduce_baseline_results/submission_final.json",
-)
-
 with open(
     asl_predictions_path,
     "r",
 ) as reader:
     asl_predictions = json.load(reader)["detect_results"]
 
-for clip_id in tqdm(list(asl_predictions.keys())):
-    clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[clip_id] = dict()
+
+with open(analysis_data_path, "rb") as reader:
+    analysis_data = pickle.load(reader)
+
+
+def postprocess_asl_predictions_per_clip(
+    clip_id: str,
+    asl_predictions: Dict[str, List[Dict[str, Any]]],
+    distinct_ground_truth_labels: List[str],
+):
+    frame_id_asl_predicted_label_indices_and_scores_mapping = dict()
     cap = cv2.VideoCapture(
         os.path.join(os.environ["SCRATCH"], "ego4d_data/v2/clips", clip_id + ".mp4")
     )
@@ -39,13 +55,11 @@ for clip_id in tqdm(list(asl_predictions.keys())):
     fps = float(cap.get(cv2.CAP_PROP_FPS))
     cap.release()
     for frame_id in range(frame_count):
-        clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[clip_id][
-            frame_id
-        ] = dict()
+        frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id] = dict()
         for label_index in range(len(distinct_ground_truth_labels)):
-            clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[clip_id][
-                frame_id
-            ][label_index] = 0.0
+            frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id][
+                label_index
+            ] = 0.0
         current_frame_time = frame_id / fps
         assigned_label_to_current_frame = False
         annotations = asl_predictions[clip_id]
@@ -62,38 +76,58 @@ for clip_id in tqdm(list(asl_predictions.keys())):
                 and annotation_end_time >= current_frame_time
             ):
                 assigned_label_to_current_frame = True
-                clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[
-                    clip_id
-                ][frame_id][annotation_label_index] = max(
-                    clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[
-                        clip_id
-                    ][frame_id][annotation_label_index],
+                frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id][
+                    annotation_label_index
+                ] = max(
+                    frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id][
+                        annotation_label_index
+                    ],
                     annotation_score,
                 )
         if not assigned_label_to_current_frame:
-            clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[clip_id][
-                frame_id
-            ][0] = 1.0
+            frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id][0] = 1.0
 
         # Normalize scores per frame so that their sum is equal to 1.0.
         sum_scores = 0.0
         for label_index in range(len(distinct_ground_truth_labels)):
-            sum_scores += (
-                clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[
-                    clip_id
-                ][frame_id][label_index]
-            )
+            sum_scores += frame_id_asl_predicted_label_indices_and_scores_mapping[
+                frame_id
+            ][label_index]
         for label_index in range(len(distinct_ground_truth_labels)):
-            clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[clip_id][
-                frame_id
-            ][
+            frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id][
                 label_index
-            ] = clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping[
-                clip_id
-            ][
-                frame_id
-            ][
+            ] = frame_id_asl_predicted_label_indices_and_scores_mapping[frame_id][
                 label_index
             ] / float(
                 sum_scores
             )
+
+
+clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping = pqdm(
+    dict(
+        [
+            {
+                "clip_id": clip_id,
+                "asl_predictions": asl_predictions,
+                "distinct_ground_truth_labels": distinct_ground_truth_labels,
+            }
+            for clip_id in list(
+                analysis_data["clip_id_frame_id_label_indices_mapping"].keys()
+            )
+        ]
+    ),
+    function=postprocess_asl_predictions_per_clip,
+    n_jobs=8,
+    argument_type="kwargs",
+    exception_behaviour="immediate",
+)
+
+with open(
+    os.path.join(
+        os.environ["SCRATCH"],
+        "ego4d_data/v2/analysis_data",
+        "clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping.pickle",
+    ),
+    "wb",
+) as writer:
+    pickle.dump(clip_id_frame_id_asl_predicted_label_indices_and_scores_mapping, writer)
