1051a98bddf7c4c2b61f2855812f04926bf7ea41
Arda Arslan <aarslan@robustus.ee.ethz.ch>
2023-11-28 17:08:42 +0100

    Updates

--- scripts/06_analyze_frame_features/06_fine_tune_blip2_frame_wise/02_fine_tune_blip2_frame_wise.sh
+++ scripts/06_analyze_frame_features/06_fine_tune_blip2_frame_wise/02_fine_tune_blip2_frame_wise.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 02_fine_tune_blip2_frame_wise.py
+CONDA_OVERRIDE_CUDA=11.7 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 02_fine_tune_blip2_frame_wise.py
+
