2aab5c73ebcda3ae8bfb79dd235b8bad98be2076
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2023-12-07 10:49:11 +0100

    Updates

--- scripts/07_blip2_embedding_analysis/01_extract_frame_features.sh
+++ scripts/07_blip2_embedding_analysis/01_extract_frame_features.sh
@@ -3,9 +3,10 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -f FRAME_FEATURE_NAME -q QUARTER_INDEX"
+   echo "Usage: $0 -f FRAME_FEATURE_NAME -q QUARTER_INDEX -s SPLIT"
    echo -e "\t-f One of blip2_vqa, video_blip."
-   echo -e "\t-q One of 0, 1, 2, 3, 4, 5"
+   echo -e "\t-q One of 0, 1, 2, 3, 4, 5."
+   echo -e "\t-s One of train, val, test."
    exit 1 # Exit script after printing help
 }
 
@@ -14,15 +15,16 @@ do
    case "$opt" in
       f ) FRAME_FEATURE_NAME="$OPTARG" ;;
       q ) QUARTER_INDEX="$OPTARG" ;;
+      s ) SPLIT="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$FRAME_FEATURE_NAME" ] || [ -z "$QUARTER_INDEX" ]
+if [ -z "$FRAME_FEATURE_NAME" ] || [ -z "$QUARTER_INDEX" ] || [ -z "$SPLIT" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
 fi
 
-python3 01_extract_frame_features.py --frame_feature_name $FRAME_FEATURE_NAME --quarter_index $QUARTER_INDEX
+python3 01_extract_frame_features.py --frame_feature_name $FRAME_FEATURE_NAME --quarter_index $QUARTER_INDEX --split $SPLIT
