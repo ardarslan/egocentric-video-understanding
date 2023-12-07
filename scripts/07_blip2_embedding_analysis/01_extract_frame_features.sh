86bac2c63b5cd5670ab8cf17a7714f8182d8088c
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2023-12-07 10:40:28 +0100

    Run on CVL

--- scripts/07_blip2_embedding_analysis/01_extract_frame_features.sh
+++ scripts/07_blip2_embedding_analysis/01_extract_frame_features.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,4 python3 01_extract_frame_features.py --frame_feature_name blip2_vqa --quarter_index 0
-CUDA_VISIBLE_DEVICES=1,5 python3 01_extract_frame_features.py --frame_feature_name blip2_vqa --quarter_index 1
-CUDA_VISIBLE_DEVICES=2,6 python3 01_extract_frame_features.py --frame_feature_name blip2_vqa --quarter_index 2
-CUDA_VISIBLE_DEVICES=3,7 python3 01_extract_frame_features.py --frame_feature_name blip2_vqa --quarter_index 3
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -f FRAME_FEATURE_NAME -q QUARTER_INDEX"
+   echo -e "\t-f One of blip2_vqa, video_blip."
+   echo -e "\t-q One of 0, 1, 2, 3, 4, 5"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "f:q:" opt
+do
+   case "$opt" in
+      f ) FRAME_FEATURE_NAME="$OPTARG" ;;
+      q ) QUARTER_INDEX="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$FRAME_FEATURE_NAME" ] || [ -z "$QUARTER_INDEX" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+python3 01_extract_frame_features.py --frame_feature_name $FRAME_FEATURE_NAME --quarter_index $QUARTER_INDEX
