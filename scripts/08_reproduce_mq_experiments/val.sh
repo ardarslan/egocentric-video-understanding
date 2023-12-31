d65daae0008b3bcfadb5998e5f0ed1d9bf435f25
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2023-12-31 14:13:52 +0100

    Updates

--- scripts/08_reproduce_mq_experiments/val.sh
+++ scripts/08_reproduce_mq_experiments/val.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 eval.py --config /home/aarslan/mq/scripts/08_reproduce_mq_experiments/configs/baseline.yaml --topk 1 --ckpt /srv/beegfs-benderdata/scratch/aarslan_data/data/checkpoints/ego4d_asl_features/epoch_014.pth.tar
+python3 eval.py --config /home/aarslan/mq/scripts/08_reproduce_mq_experiments/configs/ego4d_asl_features.yaml --topk 1 --ckpt /srv/beegfs-benderdata/scratch/aarslan_data/data/checkpoints/ego4d_asl_features/epoch_014.pth.tar
