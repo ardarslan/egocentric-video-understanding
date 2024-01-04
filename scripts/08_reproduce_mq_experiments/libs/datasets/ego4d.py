0069e0428e45ede10e1d83c19a24e9b7414c8121
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2024-01-04 11:07:55 +0100

    Removed prints

--- scripts/08_reproduce_mq_experiments/libs/datasets/ego4d.py
+++ scripts/08_reproduce_mq_experiments/libs/datasets/ego4d.py
@@ -401,11 +401,6 @@ class Ego4dDataset(Dataset):
                         assert caption_sbert_embedding.shape[1] == 768
                         frame_feats[index].append(caption_sbert_embedding)  # (1, 768)
 
-        print("len(frame_feats[0]):", len(frame_feats[0]))
-        print("len(frame_feats[1]):", len(frame_feats[1]))
-        print("[a.shape for a in frame_feats[0]]:", [a.shape for a in frame_feats[0]])
-        print("[b.shape for b in frame_feats[1]]:", [b.shape for b in frame_feats[1]])
-
         for i in range(len(self.frame_feat_names)):
             frame_feats[i] = np.vstack(
                 frame_feats[i]
