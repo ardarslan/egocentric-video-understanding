8d7244b6deb46fae283d1b63a67dacf4ad35edb0
Arda Arslan <aarslan@ait-server-03.inf.ethz.ch>
2024-01-08 11:29:13 +0100

    Added missing dones

--- scripts/09_reproduce_per_frame_fcnn_experiments/commands.sh
+++ scripts/09_reproduce_per_frame_fcnn_experiments/commands.sh
@@ -9,3 +9,5 @@ sleep_if_necessary() {
 for NUM_NONLINEAR_LAYERS in 0 1 2; do
     for CONFIG_FILE_NAME_WO_EXT in "ego4d_asl_features" "proposed_features_v2" "proposed_features_v5"; do
         sbatch --time 720 --gres=gpu:1 --cpus-per-task=1 --mem 60G  01_reproduce_per_frame_fcnn_experiments.sh --num_nonlinear_layers $NUM_NONLINEAR_LAYERS --config_file_name_wo_ext $CONFIG_FILE_NAME_WO_EXT
+    done
+done
